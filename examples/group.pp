4f2773aaae7ccaffb2b5645143f275021af4d6ed
Lennart Betz <lennart.betz@netways.de>
2020-07-17 07:38:39 +0200

    rework blocks param to Array instead of Hash

--- examples/group.pp
+++ examples/group.pp
@@ -1,24 +1,32 @@
 csync2::group { 'cluster':
   hosts  => ['node1.example.org', 'node2.example.org'],
-  blocks => {
-    0 => {
+  blocks => [
+    {
       'includes' => [ '/etc/csync2.cfg' ],
     },
-  },
+  ],
   key    => 'supersecret',
 }
 
 csync2::group { 'monitoring':
   hosts  => ['node1.example.org', 'node2.example.org'],
-  blocks => {
-    0 => {
+  blocks => [
+    {
       'includes' => [ '/etc/icinga2/features-available', '/etc/icinga2/features-enabled' ],
+      'actions'  => [
+        {
+          'pattern' => [ '/etc/icinga2/features-enabled/*' ],
+          'exec'    => [ 'systemctl reload icinga2' ],
+          'logfile' => '/var/log/csync2_action.log',
+          'do'      => 'do-local',
+        },
+      ],
     },
-    1 => {
+    {
       'includes' => [ '/etc/icingaweb2' ],
       'excludes' => [ '/etc/icingaweb2/modules/director', '/etc/icingaweb2/enabledModules/director' ],
     },
-  },
+  ],
   key    => 'supersecret',
 }
 
--- types/groupblock.pp
+++ types/groupblock.pp
@@ -1,2 +1,2 @@
 # A strict type for group blocks
-type Csync2::GroupBlock = Struct[{ includes => Array[String], Optional[excludes] => Array[String], Optional[actions] => Hash[Integer,Csync2::GroupBlockAction] }]
+type Csync2::GroupBlock = Struct[{ includes => Array[String], Optional[excludes] => Array[String], Optional[actions] => Array[Csync2::GroupBlockAction] }]
